1758ded6571e321d6a7c36683b7eae6cb7c27b28
samarvir <samarvir1996@gmail.com>
2020-03-08 14:21:40 +0530

    Add: color, type, shadow, borderRadius

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import HookForm from "./HookForm";
+import { Heading, Box, Button } from "@chakra-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={4} background={"white"}>
+      <Heading>Heading 1</Heading>
+      <HookForm />
+      <br />
+      <Button borderRadius={"sm"} boxShadow={"xl"}>
+        Submit
+      </Button>
+
+      <br />
+      <br />
+      <br />
+
+      <Box bg="tomato" w="100%" borderRadius={"sm"} p={4} color="white">
+        This is the Box
+      </Box>
+    </Box>
   );
 }
-
 export default App;
